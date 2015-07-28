85236acece2704ab986e1db96ac50390183093bc
rajsingh-maverick <rajsingh.maverick@gmail.com>
2015-07-28 21:51:23 +0530

    First Check in

---
+++ ELearning.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELearning.Web.Startup))]
+namespace ELearning.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
